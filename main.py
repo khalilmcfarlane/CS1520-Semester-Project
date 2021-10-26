ecdd030fdba570bb8be7c0df965dc39d7e62b51e
Khalil McFarlane <kmac36960@gmail.com>
2021-10-26 18:27:26 -0400

    Added google-cloud-datastore to requirements

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = flask.Flask(__name__)
 def root():
     return flask.redirect("/s/index.html", code=302)
 
+@app.route('/login')
+def login():
+    return flask.render_template("/s/login.html")
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
