178c2f2249c4c661648e8dc88960270571ff9884
Khalil McFarlane <kmac36960@gmail.com>
2021-10-28 12:01:13 -0400

    Finishing store_entity and fetch_entity functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 from flask import url_for
+from flask.globals import request
 from google.cloud import datastore
 
 app = flask.Flask(__name__)
@@ -10,14 +11,23 @@ client = datastore.Client()
     More reliable than JS, which is easy to change the source code.
     Will use in signup()
 '''
-def store_entity():
-    pass
+def store_entity(username, password):
+    key = client.key('User', username)
+    entity = datastore.Entity(key=key)
+    entity.update({
+        username: password,
+    })
+    client.put(entity)
+
+
 '''
     Retrieves stored entity.
     Useful in testing in user verification during login
 '''
 def fetch_entity():
-    pass
+    query=client.query(kind='User')
+    results = list(query)
+    return results
 
 @app.route('/')
 def root():
@@ -29,14 +39,19 @@ def login():
         pass
         #return flask.redirect(url_for("Profile", username=flask.session["username"]))
     elif flask.request.method == "POST":
-        if flask.request.form["user"] in entity:
-            pass
+        results = fetch_entity()
+        if flask.request.form["username"] in results:
+            for elem in results:
+                if elem == flask.request.form["username"]: 
+                    return flask.redirect(url_for("root", username=flask.session["username"]))
             # Find entity key (password)
             #return redirect to profile
     return flask.render_template("/s/login.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if flask.request.method == "POST":
+        store_entity(flask.request.form["username"], flask.request.form["password"])        
     return flask.render_template("/s/signup.html")
 
 if __name__ == '__main__':
