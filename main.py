a4aec8d089320adcc1a780ad9befbe0d3243b684
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-10-29 19:22:31 -0400

    Add register_user function

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ client = datastore.Client()
 def root():
     return flask.redirect("/s/index.html", code=302)
 
+@app.route('/send', methods=['POST','GET'])
+def register_user():
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    um.register(username,password) # register the new user
+    return flask.render_template("profile.html", username = username)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
