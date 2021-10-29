00d1846519721dd6744b64b5dbea4b3f966ec9f6
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-10-29 19:20:27 -0400

    Create user.py

---
+++ user.py
@@ -0,0 +1,61 @@
+from google.cloud import datastore
+
+def get_client():
+    return datastore.Client()
+
+def create_user():
+    client = get_client()
+    key = client.key('user')
+    return datastore.Entity(key)
+
+
+class User_manager():
+    """An object representing a single chat message."""
+    def find_user(self, username):
+        client = get_client()
+        query = client.query(kind = 'user')
+        query.add_filter("username", "=", username) # find the specified username 
+
+        if query.fetch.asList.size() == 0:
+            return None
+
+        for entity in query.fetch():
+            user = entity
+        return user
+
+    def register(self, username, password):
+        """Register a new user"""
+        user = self.find_user(username)
+        if user is not None:
+            return "Username is taken"
+
+        user = create_user()
+        user['username'] = username
+        user['password'] = password
+        # these user fields can be added later
+        # user['name'] = name
+        # user['age'] = age
+        # user['email'] = email
+        # user['location'] = location
+        # user['bio'] = bio
+        client = get_client()
+        client.put(user)
+        #add check for when username is satisfied
+
+    def login(self, username, password):
+        """Find a registed user"""
+        user = self.find_user(username)
+        if user == None:
+            return "User Not Found"
+
+        if user['password'] == password:
+            return "Log In Succeeded"
+        else:
+            return "Wrong Password"
+
+
+
+
+        
+
+    
