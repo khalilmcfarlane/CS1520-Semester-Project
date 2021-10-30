180ed849dcf3e2e8c34d8a66b84aca8ae7430c0a
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-10-29 20:34:26 -0400

    Login and Register

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import flask
+import user
 from google.cloud import datastore
+
+um = user.User_manager()
 
 app = flask.Flask(__name__)
 client = datastore.Client()
@@ -13,8 +16,18 @@ def root():
 def register_user():
     username = flask.request.form['username']
     password = flask.request.form['password']
-    um.register(username,password) # register the new user
+    um.register(username, password) # register the new user
+    print("registered username", username)
     return flask.render_template("profile.html", username = username)
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    um.register(username, password) # register the new user
+    print("registered username", username)
+    return flask.render_template("profile.html", username = username)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
