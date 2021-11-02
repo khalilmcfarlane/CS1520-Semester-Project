6108bb2a55e7b2c0b1d022d0bbd7997bc8fe4ae6
Khalil McFarlane <kmac36960@gmail.com>
2021-11-01 22:27:04 -0400

    Form validation should work with datastore, but it's not submitting

--- forms_manager.py
+++ forms_manager.py
@@ -1,6 +1,10 @@
-from flask_wtf import FlaskForm
 from wtforms import Form, StringField, validators, TextAreaField
+from wtforms.validators import Required
 
+
+class LoginForm(Form):
+    openid = TextField('openid', validators = [Required()])
+    remember_me = BooleanField('remember_me', default = False)
 class NewPostForm(Form):
     title=StringField('Title', [validators.DataRequired()])
     article=TextAreaField('Article', [validators.DataRequired()])
